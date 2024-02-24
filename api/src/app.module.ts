5aba26d630ddbb4b1a540ab9535fe08e88de6b46
Ahmed Abbas <theabbos@gmail.com>
2024-02-23 23:29:52 -0500

    adds the boiler plate code to create the api

---
+++ api/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
