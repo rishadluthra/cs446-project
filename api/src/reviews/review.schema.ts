ec879bf75b2d140a876c4b9870859ff66bd5838e
Simon Zuccherato <46201929+simonzuccherato@users.noreply.github.com>
2024-03-31 23:57:17 -0400

    Reviews backend (#5)
    Add reviews to backend

---
+++ api/src/reviews/review.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
+
+export type ReviewDocument = HydratedDocument<Review>;
+
+@Schema({ timestamps: true })
+export class Review {
+  id: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User', required: true })
+  creatorId: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User', required: true })
+  targetId: string;
+
+  @Prop({ required: true })
+  rating: number;
+
+  @Prop({ required: false })
+  review: string;
+
+  createdAt: Date;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
